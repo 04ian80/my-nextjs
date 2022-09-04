8951c456de6d618c0311f499213cb2ea3e8b67bb
Ain Chung <artnouveau0804@gmail.com>
2022-09-05 02:41:28 +0900

    [2] Routing (useRaouter)
    useRouter라는 기능을 써보면 routing된 path와 현재 페이지의 정보들이 뜸. 여기서 style을 적용할 때 각자의 링크에 와있다면 색깔 블랙을, 아니라면 다른 색상을 넣어서 현재페이지를 알 수 있게 해줌.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,28 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
   return (
     <nav>
       <Link href='/'>
-        <a>Home</a>
+        <a
+          style={{
+            color: router.pathname === '/' ? 'black' : 'rgba(0,0,0,0.6)',
+          }}
+        >
+          Home
+        </a>
       </Link>
       <Link href='/about'>
-        <a>About</a>
+        <a
+          style={{
+            color: router.pathname === '/about' ? 'black' : 'rgba(0,0,0,0.6)',
+          }}
+        >
+          About
+        </a>
       </Link>
     </nav>
   );
