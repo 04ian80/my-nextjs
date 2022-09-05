34482d1a39b7df0147864f898b4dd86880e1898c
Ain Chung <artnouveau0804@gmail.com>
2022-09-06 01:26:08 +0900

    [7] Fetching Data
    public 폴더에 파일을 넣으면 어디선가 그 파일을 가져올 때 경로를 슬래쉬 하나만 넣어서 쉽게 가져올 수 있음.
    
    data를 가져올 때 await (await fetch(URL)).json 이 코드를 (async () => {})();이 비동기 함수에 넣고 이 비동기 함수를 useEffect(() => {}, [])에 넣는다.
    
    url에서 받아온 data 객체를 보면 results라는 key안에 영화의 배열이 담겨있음. 여기서 result만 가져와서 상태값을 results로 바꿔준다음, jsx로 뿌려주면 됨.
    여기서 만약 초기값을 아무것도 안넣어주면 에러가 남.
    
    movies.map을 돌릴때 movies뒤에 ?를 붙혀주면 '데이터가 받아와지면 렌더링해라' 라는 뜻이 됨. 안받아와지면 'Loading...'이 뜨게 해줌.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
+const API_KEY = '8b282bd74c8ec8d87741915ecfc8adbf';
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(URL)).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title='Home' />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
