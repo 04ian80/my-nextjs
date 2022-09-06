8808506337d8cd110dc23069e6b2447be962bfac
Ain Chung <artnouveau0804@gmail.com>
2022-09-07 04:25:35 +0900

    [14] Movie Detail (1)
    1. 영화를 클릭하면 상세페이지로 이동해야한다. 영화 포스터를 누르면 이동하게 하려 했지만 <a>태그 안에 <div>태그가 있는 것은 문법적으로 맞지 않기 때문에 일단 영화 제목에만 링크를 걸어두고, 영화 포스터에는 onClick이벤트를 건다. onClick이벤트 함수에 movie.id를 전달 한 뒤, router.push라는 것으로 url을 전달한다.
    
    2. 상세페이지로 이동하려면 url을 movie/popular가 아닌 movie/{movie.id}로 해줘야 하기 때문에 next.config의 Rewrites에서 source와 destination을 입력해준다. 여기서 source의 movie/뒤에 이름과 destination에서 movie/뒤에 이름을 같게 해줘야 한다. 나의 경우 movie/:id

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import Seo from '../components/Seo';
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`/movies/${id}`);
+  };
   const [movies, setMovies] = useState([]);
   return (
     <div className='container'>
       <Seo title='Home' />
       {results?.map((movie) => (
-        <div key={movie.id} className='movie'>
+        <div onClick={() => onClick(movie.id)} key={movie.id} className='movie'>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <Link href={`/movies/${movie.id}`}>
+            <a>{movie.original_title}</a>
+          </Link>
         </div>
       ))}
       <style jsx>{`
