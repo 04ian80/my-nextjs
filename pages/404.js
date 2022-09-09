da223e85abaa92695799447f96a9c881e76ef789
Ain Chung <artnouveau0804@gmail.com>
2022-09-10 01:50:35 +0900

    [18] 404 Not Found 페이지
    404 Not Found 페이지를 커스터마이징 하려면 pages폴더에서 404.js 파일을 만들고, 그 안에 페이지를 못찾았을 때 나타낼 페이지를 만들어주면 된다.

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>404 Not Found</h1>
+      <style jsx>{`
+        h1 {
+          display: flex;
+          justify-content: center;
+        }
+      `}</style>
+    </>
+  );
+}
